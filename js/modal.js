5cfeba739753e5bd3bc96f92573ea3fb95253a83
EmaxxD <97693129+EmaxxD@users.noreply.github.com>
2023-11-13 21:05:47 +0200

    last

--- js/modal.js
+++ js/modal.js
@@ -12,18 +12,3 @@
     refs.modal.classList.toggle("is-open");
   }
 })();
-//                                 MOBILE MENU
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-menu-open]"),
-    closeModalBtn: document.querySelector("[data-menu-close]"),
-    modal: document.querySelector("[data-burger]"),
-  };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-open");
-  }
-})();
